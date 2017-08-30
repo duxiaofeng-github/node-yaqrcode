883cf36c68f4e61b54163ff1e484510e812264c9
duxiaofeng <297951292@qq.com>
2017-08-30 19:48:26 +0800

    fix typing

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,4 @@ interface YaQrcodeStatic {
     (str: string, opt?: { size: number }): string;
 }
 
-declare module "yaqrcode" {
-    export var yaqrcode: YaQrcodeStatic;
-    export default YaQrcodeStatic;
-}
+export var yaqrcode: YaQrcodeStatic;
